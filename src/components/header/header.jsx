ae34db6762c32d0118e9cde688c07ecf6e19e5b6
Wizqqr <baban1979molotov@gmail.com>
2023-05-21 11:53:36 +0600

    react

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className='header'
+>
+    <nav>
+        <a href="#" className='LINK'>home</a>
+        <a href="#" className='LINK'>about</a>
+        <a href="#" className='LINK'>news</a>
+        <a href="#" className='LINK'>novinki</a>
+        </nav>
+</div>    )
+}
+export default Header
